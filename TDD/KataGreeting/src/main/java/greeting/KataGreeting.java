e518e5d33bf27ad40bb8df0f1db2649935686a0a
LucasHope <anttipoyhonen1986@gmail.com>
2017-11-01 20:19:37 +0200

    greeting kata 01

--- TDD/KataGreeting/src/main/java/greeting/KataGreeting.java
+++ TDD/KataGreeting/src/main/java/greeting/KataGreeting.java
@@ -1,12 +1,34 @@
 package greeting;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class KataGreeting {
 
-    public String greet(String name) {
-        return "";
+    public String greet(String...names) {
+        
+        String print;
+        String printname;
+        String lastname;
+        
+        if (names == null) printname = "friend";
+        else {
+                
+            List<String> allnames = Arrays.asList(names);
+                
+            lastname = allnames.remove(allnames.size() - 1);
+            
+            printname = String.join(", ", allnames) + " and " + lastname;
+        }
+        
+        print = "Hello, " + printname + ".";
+        
+        if (printname.toUpperCase().equals(printname)) {
+            print = print.toUpperCase().replace(".", "!");
+        }
+        
+        return print;
+        
     }
     
 }
--- TDD/KataGreeting/src/test/java/greeting/KataGreetingTest.java
+++ TDD/KataGreeting/src/test/java/greeting/KataGreetingTest.java
@@ -10,7 +10,23 @@ public class KataGreetingTest {
                 
     @Test
     public void testBasicGreeting() {   
-        assertEquals("Hello, Tommi.", g.greet("Tommi"));   
+        assertEquals("Hello, Tommi.", g.greet("Tommi"));  
+        assertEquals("Hello, Kalle.", g.greet("Kalle"));  
+    }
+    
+    @Test
+    public void testNullGreeting() {
+        assertEquals("Hello, friend.", g.greet(null));
+    }
+    
+    @Test
+    public void testShoutGreeting() {
+        assertEquals("HELLO, TOMMI!", g.greet("TOMMI"));
+    }
+    
+    @Test
+    public void testMultipleGreeting() {
+        assertEquals("Hello, Tommi and Kalle.", g.greet("Tommi", "Kalle"));
     }
     
     
