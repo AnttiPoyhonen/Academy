f8486e0b305efa54482463b6ba34c00e34990414
LucasHope <anttipoyhonen1986@gmail.com>
2017-11-01 13:01:16 +0200

    tomcat dl

--- TDD/KataGreeting/src/test/java/greeting/KataGreetingTest.java
+++ TDD/KataGreeting/src/test/java/greeting/KataGreetingTest.java
@@ -11,40 +11,7 @@ public class KataGreetingTest {
     @Test
     public void testBasicGreeting() {   
         assertEquals("Hello, Tommi.", g.greet("Tommi"));   
-        assertEquals("Hello, Samu.", g.greet("Samu"));      
     }
-               
-    @Test
-    public void testNullGreeting() {   
-        assertEquals("Hello, friend.", g.greet(null));      
-    }
-    
-    @Test
-    public void testShoutGreeting() {   
-        assertEquals("HELLO, MIKA.", g.greet("MIKA"));      
-    }
-    
-    @Test
-    public void testMultipleGreeting() {   
-        assertEquals("Hello, Jari and Josefin.", g.greet("Jari", "Josefin")); 
-        assertEquals("Hello, Jake and Minna.", g.greet("Jake", "Minna"));    
-        assertEquals("Hello, Tuukka, Risto and Kristiina.", g.greet("Tuukka", "Risto", "Kristiina"));
-    }
-    
-    @Test
-    public void testMultipleAndShoutingGreeting() {
-        assertEquals("Hello, Tuukka, Risto and Minna. AND HELLO, KRISTIINA AND JARI!",
-                g.greet("Tuukka", "KRISTIINA", "JARI", "Risto", "Minna"));
-    }
-    
-//    
-//    @Test
-//    public void testGreeting() {
-//        String expResult = "";
-//        String result = g.greeting();        
-//        assertEquals(expResult, result);
-//        
-//    }
     
     
 }
